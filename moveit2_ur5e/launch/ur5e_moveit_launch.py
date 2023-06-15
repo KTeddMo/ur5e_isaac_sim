ccd3504210819f9900cdde2c964726cb442b0a9c
KTeddMo <114058853+KTeddMo@users.noreply.github.com>
2023-06-16 01:10:26 +0900

    Update ur5e_moveit_launch.py

--- moveit2_ur5e/launch/ur5e_moveit_launch.py
+++ moveit2_ur5e/launch/ur5e_moveit_launch.py
@@ -91,7 +91,6 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
-        namespace='move',
         output="both",
         parameters=[moveit_config.robot_description],
         remappings=[("/tf","ur5e_tf")],
