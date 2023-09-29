0e7ead4c9ef57d20e002564c9f6b281946a4066f
KTeddMo <114058853+KTeddMo@users.noreply.github.com>
2023-09-29 17:07:18 +0900

    Create ur5e_moveit.launch.py

---
+++ ur5e_moveit.launch.py
@@ -0,0 +1,148 @@
+import os
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+from ament_index_python.packages import get_package_share_directory
+
+import xacro
+
+
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+
+def generate_launch_description():
+    #moveit_config = MoveItConfigsBuilder("panda", package_name="moveit_panda").to_moveit_configs()
+    #return generate_demo_launch(moveit_config)
+
+    ros2_control_hardware_type = DeclareLaunchArgument(
+        "ros2_control_hardware_type",
+        default_value="ur5e",
+        description="ROS2 control hardware interface type to use for the launch file -- possible values: [mock_components, isaac]",
+    )
+
+    moveit_config = (
+        MoveItConfigsBuilder("ur5e_gripper", package_name="moveit2_ur5e")
+        .robot_description(
+            file_path="config/ur5e_gripper.urdf.xacro",
+            mappings={
+                "ros2_control_hardware_type": LaunchConfiguration(
+                    "ros2_control_hardware_type"
+                )
+            },
+        )
+        .robot_description_semantic(file_path="config/ur5e_gripper.srdf")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
+        )
+        .planning_scene_monitor(
+        publish_robot_description=True, publish_robot_description_semantic=True
+        )
+        .to_moveit_configs()
+    )
+    
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
+        parameters=[moveit_config.to_dict()],
+        arguments=["--ros-args", "--log-level", "info"],
+    )
+    rviz_base = os.path.join(
+        get_package_share_directory("moveit2_ur5e"), "config"
+    )
+    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_full_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+        ],
+    )
+    # Static TF
+    static_tf_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_transform_publisher",
+        output="log",
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+    )
+
+    
+    # Publish TF
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="both",
+        parameters=[moveit_config.robot_description],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("moveit2_ur5e"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[moveit_config.robot_description, ros2_controllers_path],
+        output="screen",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    ur5e_arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur5e_arm_controller", "-c", "/controller_manager"],
+    )
+
+    ur5e_hand_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ur5e_hand_controller", "-c", "/controller_manager"],
+    )
+
+
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            move_group_node,
+            ros2_control_hardware_type,
+            robot_state_publisher,
+            static_tf_node,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            ur5e_arm_controller_spawner,
+            ur5e_hand_controller_spawner,
+            generate_move_group_launch(moveit_config),
+        ]
+    )
