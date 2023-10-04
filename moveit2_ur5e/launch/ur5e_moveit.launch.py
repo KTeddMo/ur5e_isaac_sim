96dc5935f2c71c1eb5cd9ba623f7aed7fe2aca34
KTeddMo <114058853+KTeddMo@users.noreply.github.com>
2023-10-04 21:35:59 +0900

    Update ur5e_moveit.launch.py

--- moveit2_ur5e/launch/ur5e_moveit.launch.py
+++ moveit2_ur5e/launch/ur5e_moveit.launch.py
@@ -43,7 +43,7 @@ def generate_launch_description():
         .robot_description_kinematics(file_path="config/kinematics.yaml")
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .planning_pipelines(
-            pipelines=["ompl", "pilz_industrial_motion_planner"]
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
         )
         .planning_scene_monitor(
         publish_robot_description=True, publish_robot_description_semantic=True
