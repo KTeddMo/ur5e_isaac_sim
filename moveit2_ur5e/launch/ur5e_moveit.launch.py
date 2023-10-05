a0ae2c8a59756c9cf078c5f75fe59d39bf3e4e33
KTeddMo <114058853+KTeddMo@users.noreply.github.com>
2023-10-05 14:22:25 +0900

    Update ur5e_moveit.launch.py

--- moveit2_ur5e/launch/ur5e_moveit.launch.py
+++ moveit2_ur5e/launch/ur5e_moveit.launch.py
@@ -31,14 +31,7 @@ def generate_launch_description():
 
     moveit_config = (
         MoveItConfigsBuilder("ur5e_gripper", package_name="moveit2_ur5e")
-        .robot_description(
-            file_path="config/ur5e_gripper.urdf.xacro",
-             mappings={
-                "ros2_control_hardware_type": LaunchConfiguration(
-                    "ros2_control_hardware_type"
-                )
-            },
-        )
+        .robot_description(file_path="config/ur5e_gripper.urdf.xacro")
         .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .planning_pipelines(
             pipelines=["ompl", "pilz_industrial_motion_planner"]
